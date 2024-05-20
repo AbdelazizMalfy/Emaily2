647620a0b48133868683c84e3dde9076a45ca640
abdelazizmalfy <abdelazizmalfy@gmail.com>
2024-05-20 16:49:31 +0200

    fix sameSite policy

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === "production",
       maxAge: 30 * 24 * 60 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: "none",
       httpOnly: false,
     },
   })
